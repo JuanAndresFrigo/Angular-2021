9f35704279246f00eef8e726d4948311ae9f8cb4
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-05-24 14:50:04 -0300

    First commit

---
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  constructor(public dbzService: DbzService) {
+  }
+
+  agregarNuevoPersonaje(argumento: Personaje) {
+    // this.personajes.push(argumento);
+  }
+
+}
