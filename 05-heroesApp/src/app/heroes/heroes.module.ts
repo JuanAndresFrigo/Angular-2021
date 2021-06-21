99d7d1b3a4208d8595aa0030e7d373268f5c46dd
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-06-20 23:27:27 -0300

    fin seccion 13

--- 05-heroesApp/src/app/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -9,6 +9,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../material/material.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,14 @@ import { MaterialModule } from '../material/material.module';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
+    HeroeTarjetaComponent,
+    ImagenPipe
   ],
   imports: [
     CommonModule,
     HeroesRoutingModule,
+    FormsModule,
     MaterialModule,
     FlexLayoutModule
   ]
--- 05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
+++ 05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Heroe } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -6,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+
+  heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe | undefined;
+
+  constructor(private heroeService: HeroesService) { }
 
   ngOnInit(): void {
   }
 
+  buscando() {
+    this.heroeService.getSugerencia(this.termino)
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+    console.log("event: ", event.option.value);
+
+    if (!event.option.value) {
+      this.termino = 'Termino no encontrado';
+      return;
+    }
+
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroeService.getHeroePorId(heroe.id!)
+      .subscribe(heroe => this.heroeSeleccionado = heroe);
+
+  }
 }
