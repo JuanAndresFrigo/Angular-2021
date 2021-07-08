c41b3fab86fa09250e6e029596bb5f50ef8f2f2a
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-07-07 21:16:58 -0300

    fin seccion 16

---
+++ 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
+@Component({
+  selector: 'app-dinamicos',
+  templateUrl: './dinamicos.component.html',
+  styleUrls: []
+})
+export class DinamicosComponent {
+
+  nuevoJuego: string = '';
+
+  persona: Persona = {
+    nombre: 'Fernando',
+    favoritos: [
+      { id: 1, nombre: 'juego 1' },
+      { id: 2, nombre: 'juego 2' }
+    ]
+  }
+
+
+  guardar() {
+    console.log("formulario posteado");
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push({ ...nuevoFavorito })
+    this.nuevoJuego = '';
+  }
+}
