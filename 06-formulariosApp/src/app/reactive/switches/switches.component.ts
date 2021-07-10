14736623d888c41fa6e08ee9c7c3357051c2f070
Juan Andres Frigo <juan.a.frigo@gmail.com>
2021-07-10 17:41:24 -0300

    fin de la seccion 17 - formularios reactivos primera parte

--- 06-formulariosApp/src/app/reactive/switches/switches.component.ts
+++ 06-formulariosApp/src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,6 @@
+import { getLocaleFirstDayOfWeek } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup= this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue],
+  });
 
-  ngOnInit(): void {
+  persona={
+    genero: 'F',
+    notificaciones: true
   }
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.miFormulario.reset({...this.persona, condiciones:false});
+
+    this.miFormulario.get('condiciones')?.valueChanges.subscribe(newValue=>{
+       console.log(newValue);
+       
+    });
+
+    this.miFormulario.valueChanges.subscribe(({condiciones, ...rest})=>{
+      // delete form.condiciones;
+      this.persona = rest;
+    });
+  }
+
+  guardar(){
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+  }
 }
